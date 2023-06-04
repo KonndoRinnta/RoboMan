e64007f7353a3804a8c280356e911cc6f40dae31
近藤　倫太 <tora.goraias@gmail.com>
2023-06-04 11:31:38 +0900

    エラー修正

--- Assets/Scripts/PlayerState/AirWalkState.cs
+++ Assets/Scripts/PlayerState/AirWalkState.cs
@@ -27,7 +27,7 @@ public class AirWalkState : IPlayerState
             playerController.ChangeState(PlayerState.Dash);
         }
 
-        if(playerController.AttackInput && playerController.AirAtkAble)
+        if(playerController.AttackInput)
         {
             playerController.ChangeState(PlayerState.AirAttack);
         }
