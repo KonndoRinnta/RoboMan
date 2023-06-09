4602abc7feda72b5a1b1a2934547443041ed64c6
近藤　倫太 <tora.goraias@gmail.com>
2023-06-09 17:42:50 +0900

    ゴール処理作成中

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,11 +7,18 @@ public class GameManager : MonoBehaviour
     [SerializeField, Header("ゲームオーバーのパネル")]
     GameObject _gameOverPanel;
 
+    [SerializeField, Header("ゲームクリアのパネル")]
+    GameObject _gameClearPanel;
+
     private int _enemyKillCount;
 
     private bool _gameOver;
     public void IsGameOver()
     {
         _gameOverPanel.gameObject.SetActive(true);
+    }
+    public void IsGameCreal()
+    {
+        _gameClearPanel.gameObject.SetActive(true);
     }
 }
